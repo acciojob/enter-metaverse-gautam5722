504a0d946c9932db11e24ebfa037dcd35aa5f2cf
Gautam Kumar Sharma <81625160+gautam5722@users.noreply.github.com>
2024-12-16 20:55:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-//your JS code here. If required.
-const displayCode = document.getElementById("status");
-const btn = document.getElementById("enterBtn");
+document.addEventListener('DOMContentLoaded', () => {
+  const enterButton = document.getElementById('enterBtn');
+  const paragraph = document.getElementById('status');
 
-
-btn.addEventListener('click', () => { 
-	displayCode.outerHTML = '<h1 id="status">Entered Metaverse';
+  enterButton.addEventListener('click', () => {
+    paragraph.innerHTML = '<h1>Entered Metaverse</h1>';
+  });
 });
