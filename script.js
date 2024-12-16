a04ab2f9a346902dbd46e56f7f583929971257a1
Gautam Kumar Sharma <81625160+gautam5722@users.noreply.github.com>
2024-12-16 20:46:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
- document.getElementById("enterBtn").addEventListener("click", () => {
-      const statusParagraph = document.getElementById("status");
+//your JS code here. If required.
+const displayCode = document.getElementById("status");
+const btn = document.getElementById("enterBtn");
 
-      const newHeading = document.createElement("h1");
-      newHeading.id = "status";
-      newHeading.textContent = "Entered Metaverse"; 
 
-      statusParagraph.replaceWith(newHeading);
-    });
+btn.addEventListener('click', () => {
+	displayCode.outerHTML = '<h1 id="status">Entered Metaverse</h1>'
+})
