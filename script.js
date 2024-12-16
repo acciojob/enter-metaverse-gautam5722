2f5ee9c1d41f3b1892ccb191f16c1c7a6e0fac60
Gautam Kumar Sharma <81625160+gautam5722@users.noreply.github.com>
2024-12-16 20:27:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const displayCode = document.getElementById("status");
+const btn = document.getElementById("enterBtn");
+
+
+btn.addEventListener('click', () => {
+	displayCode.outerHTML = '<h1 id="status">Entered Metaverse</h1>';
+})
